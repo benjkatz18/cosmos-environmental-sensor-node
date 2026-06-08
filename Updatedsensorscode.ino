a30328b1b6059d222eacb49745947eb8dbe3070b
benjkatz18 <bk663@scarletmail.rutgers.edu>
2026-06-08 14:18:59 -0400

    Updatedsensorscode.ino

--- Updatedsensorscode.ino
+++ Updatedsensorscode.ino
@@ -8,10 +8,11 @@ Adafruit_BMP085 bmp;
 bool ambFound = false;
 bool pressureFound = false;
 
-void setup() {
+const int micPin = A0;
 
+void setup() {
   Serial.begin(9600);
-  while (!Serial);
+  delay(1000);
 
   Serial.println("Starting Sensors...");
 
@@ -31,15 +32,16 @@ void setup() {
 }
 
 void readAmbiMate() {
-
   if (!ambFound) {
     Serial.println("AmbiMate Not Connected");
     return;
   }
 
   if (amb.Measure()) {
-
     Serial.println("----- AmbiMate -----");
+
+    Serial.print("Raw Audio: ");
+    Serial.println(amb.AUDIO);
 
     Serial.print("Temperature (C): ");
     Serial.println(amb.TEMP);
@@ -59,13 +61,14 @@ void readAmbiMate() {
     Serial.print("Voltage: ");
     Serial.println(amb.VOLTS);
 
-    Serial.print("Motion/Event: ");
+    Serial.print("Nearby Motion: ");
     Serial.println(amb.EVENT ? "TRUE" : "FALSE");
+  } else {
+    Serial.println("AmbiMate Measurement Failed");
   }
 }
 
 void readPressureSensor() {
-
   if (!pressureFound) {
     Serial.println("Pressure Sensor Not Connected");
     return;
@@ -86,16 +89,49 @@ void readPressureSensor() {
   Serial.println(bmp.readAltitude());
 }
 
-void loop() {
+void readBOB09868Mic() {
+  int signalMax = 0;
+  int signalMin = 1023;
 
+  unsigned long startTime = millis();
+
+  while (millis() - startTime < 50) {
+    int sample = analogRead(micPin);
+
+    if (sample > signalMax) {
+      signalMax = sample;
+    }
+
+    if (sample < signalMin) {
+      signalMin = sample;
+    }
+  }
+
+  int peakToPeak = signalMax - signalMin;
+
+  Serial.println("----- BOB-09868 Microphone -----");
+
+  Serial.print("Mic Min: ");
+  Serial.println(signalMin);
+
+  Serial.print("Mic Max: ");
+  Serial.println(signalMax);
+
+  Serial.print("Sound Level Raw Peak-to-Peak: ");
+  Serial.println(peakToPeak);
+}
+
+void loop() {
   Serial.println("============================");
 
   readAmbiMate();
-
   Serial.println();
 
   readPressureSensor();
-
   Serial.println();
+
+  readBOB09868Mic();
+  Serial.println();
+
   delay(2000);
 }
